5e293724d3d7e459e574d1c28c6308fcd25f68ac
Ana Paula Souza Dias <anapaula_sdias@hotmail.com>
2021-02-27 21:22:34 -0300

    more

---
+++ src/components/Profile.tsx
@@ -0,0 +1,15 @@
+import styles from '../styles/components/Profile.module.css'
+
+export function Profile() {
+  return (
+    <div className={styles.profileContainer}>
+      <img src="https://avatars.githubusercontent.com/u/68869041?s=460&u=3f75349d52ab7f20500904f8e3f3bce3a075301d&v=4" alt="Ana Paula" />
+      <div>
+        <strong>Ana Paula Souza Dias</strong>
+        <p>
+          <img src="icons\level.svg" alt="Level" />
+         Level 1</p>
+      </div>
+    </div>
+  );
+}
